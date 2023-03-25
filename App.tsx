c3d428091fdf0817b789b5804957fc0ce1be9a7f
Yunseon Lee <67796386+Deresaanne@users.noreply.github.com>
2023-03-25 23:26:08 +0200

    Add dark mode

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
   return (
     <div className="App">
       <div>
@@ -28,8 +38,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className={`App ${theme}`}>
+        <button onClick={toggleTheme}>Toggle Theme</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
